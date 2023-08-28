88ed0f11d3200ee7c661af12e9972cc497655758
vanikulshreshtha <138274126+vanikulshreshtha@users.noreply.github.com>
2023-08-28 10:04:53 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+L=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,2,0,21,231,34,3]
+for i in L:
+    print(i)
+    if i%3==0:
+        print("element is divisible by 3")
+    else:
+        print('not divisible by 3')
